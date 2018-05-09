b4f7c1d7b80113e8f630fa26192d71bcebf43e0a
Riccardo Casazza <riccardo.casazza@gmail.com>
2018-05-09 18:08:31 +0200

    first release content

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
-ansible-galaxy install nginxinc.nginx
-ansible-galaxy install geerlingguy.certbot
+ansible-galaxy --force install nginxinc.nginx
+ansible-galaxy --force install geerlingguy.certbot
+ansible-galaxy --force install SphericalElephant.shorewall
+
 ansible-playbook -i server0.ini fragugliaserver.yml --ask-vault-pass
+
+echo Don't forget to execute the following:
+echo          Reason                               Command                       
+echo ------------------------ -------------------------------------------------- 
+echo server0 backup to cubo   server0> ssh-copy-id timemachine@cubo.casazza.eu  
+
