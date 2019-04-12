f074c7ea56d97b4908f8870022a1b6d01c58c10e
Riccardo Casazza <riccardo.casazza@gmail.com>
2019-04-12 22:05:24 +0200

    latest

--- install_server0.sh
+++ install_server0.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-ansible-galaxy --force install nginxinc.nginx
-ansible-galaxy --force install geerlingguy.certbot
+#ansible-galaxy --force install nginxinc.nginx
+#ansible-galaxy --force install geerlingguy.certbot
 #ansible-galaxy --force install SphericalElephant.shorewall
-ansible-galaxy --force install ziobombo.ansible_shorewall
+#ansible-galaxy --force install ziobombo.ansible_shorewall
 
 ansible-playbook -i servers.ini server0.yml --ask-vault-pass
 
