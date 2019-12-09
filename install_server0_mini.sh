de26b62a9780025927bce4d04ae904f240c3ba40
ziobombo <riccardo.casazza@gmail.com>
2019-12-09 12:40:03 +0100

    moved bitwarden to docker

--- install_server0_mini.sh
+++ install_server0_mini.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ansible-galaxy --force install ziobombo.ansible_shorewall
+ansible-galaxy install ziobombo.ansible_shorewall
 
 ansible-playbook -i servers.ini server0_mini.yml --ask-vault-pass
 
