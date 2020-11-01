11da013c2abc9211b12be0a628d7e0d6dca0e8d4
Riccardo Casazza <riccardo.casazza@gmail.com>
2020-11-01 23:08:11 +0100

    new releasing

--- create_version.sh
+++ create_version.sh
@@ -9,7 +9,12 @@ DEV_VERSION=`echo $CURRENT_VERSION | perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1)
 #echo release version: $RELEASE_VERSION
 #echo dev version: $DEV_VERSION
 
-curl -v -i -X POST -H "Content-Type:application/json" -u ziobombo:76b9cdc1b045058e5a6bcd9c438e611eb02eb368 https://api.github.com/repos/ziobombo/FragugliaServer/releases -d '{"tag_name":"$RELEASE_VERSION","target_commitish": "development","name": "$RELEASE_VERSION","body": "","draft": false,"prerelease": false}'
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -u "ziobombo:d2ddc65d1ae6dd2d4c43b0082c540200897c4f52" \
+  https://api.github.com/repos/ziobombo/FragugliaServer/releases \
+  -d '{"tag_name":"'$RELEASE_VERSION'","target_commitish": "development","name": "'$RELEASE_VERSION'","body": "","draft": false,"prerelease": false}'
 
 #mvn -DdevelopmentVersion=$DEV_VERSION -DreleaseVersion=$RELEASE_VERSION jgitflow:release-start
 #mvn jgitflow:release-finish
